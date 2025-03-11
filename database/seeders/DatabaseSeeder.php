f4a1f897533ff49ec836762be92e5920e805453d
Michael Agadzi <right4son@aol.com>
2025-03-11 11:42:09 +0000

    clearance

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,10 +14,18 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        User::create([
+            'type' => '',
+            'role' => '',
+            'avatar' => '',
+            'fname' => '',
+            'lname' => '',
+            'dob' => '',
+            'gender' => '',
+            'email' => 'admin@nosmay.com',
+            'contact' => '',
+            'email_verified_at' => '',
+            'password' => '',
+        ]);
     }
 }
